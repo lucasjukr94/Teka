46721bebd46b43e48dad50606144836421391245
Lucas Ju <lucasjukr94@gmail.com>
2018-11-30 16:56:15 -0200

    tryouts

--- BasicOpenTk/TekaForms/Form1.cs
+++ BasicOpenTk/TekaForms/Form1.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 using OpenTK;
 
 namespace TekaForms
 {
     public partial class Form1 : Form
     {
+        private bool loaded = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +25,44 @@ namespace TekaForms
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Show();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            if (!loaded)
+            {
+                return;
+            }
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded)
+            {
+                return;
+            }
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            //glControl1.SwapBuffers();
+        }
+
+        private void ddToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
         }
     }
 }
